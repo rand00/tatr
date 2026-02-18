47b6b79a88493e3277d37ccc463208e56e410caa
rand00 <oth.rand@gmail.com>
2026-02-18 23:46:37 +0100

    bin/Main: WIP: Added 'extract_words' + writing on 'Parse.Indentation_tree.add_line'

--- bin/main.ml
+++ bin/main.ml
@@ -73,14 +73,70 @@ module Parse = struct
       in
       n_indent, idx_visual
 
-    let add_line ~tab_is_spaces line tree =
+    (*> Helpful for choosing the right visual char's:
+        for i = 0 to 255 do 
+          CCFormat.printf "%d: '%c'\n%!" i (Char.chr i)
+        done
+     *)
+    let is_wordchar = function
+      | 'a'..'z'
+      | '_'
+      | '-'
+      | 'A'..'Z'
+      | '0'..'9' -> true
+      | _ -> false
+    
+    (*> Note: I made this interface to avoid too much extra allocation for lines*)
+    let extract_words ~include_char ~exclude_char ~from_idx line =
+      let words = ref [] in
+      let word_range = ref None in
+      let maybe_append_word () =
+        match !word_range with
+        | None -> ()
+        | Some (start, stop) ->
+          let word_str = CCString.sub line start (stop - start) in
+          words := word_str :: !words;
+          word_range := None;
+      in
+      for i = from_idx to CCString.length line -1 do
+        let c = CCString.get line i in
+        if not (exclude_char c) && (include_char c || is_wordchar c) then (
+          let word_range' = match !word_range with
+            | None -> Some (i, i+1)
+            | Some (start, stop) -> Some (start, stop+1)
+          in
+          word_range := word_range';
+        ) else (
+          maybe_append_word ()
+        )
+      done;
+      maybe_append_word ();
+      CCList.rev !words
+    
+    let add_line_to_tree
+        ~tab_is_spaces
+        ~include_char
+        ~exclude_char
+        line
+        tree =
       (*> Note that n-indent is not the same as index
         * tab is counted a specified number of spaces
         * when supporting unicode in the future, whitespace can be of even more sizes
       *)
-      let n_indent, idx_visual =
-        count_indent_until_visual ~tab_is_spaces line in
-      failwith "todo"
+      let indent, idx_visual =
+        count_indent_until_visual ~tab_is_spaces line
+      in
+      match idx_visual with
+      | None -> tree
+      | Some idx_visual ->
+        let words =
+          extract_words
+            ~include_char
+            ~exclude_char
+            ~from_idx:idx_visual
+            line
+        in
+        failwith "todo"
 
   end
 
