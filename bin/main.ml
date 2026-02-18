bfdd971d9ed69673ff7fbf0accabb2481bb78b46
rand00 <oth.rand@gmail.com>
2026-02-18 22:57:39 +0100

    bin/Main: Added Parse.Indentation_tree.count_indent_until_visual - for now only supporting ASCII

--- bin/main.ml
+++ bin/main.ml
@@ -54,6 +54,34 @@ module Parse = struct
 
   module Indentation_tree = struct
 
+    let count_indent_until_visual ~tab_is_spaces line =
+      let n_indent, _, idx_visual = 
+        let exception Shortcircuit of (int * int * int option) in
+        try
+          line |> CCString.fold_left (fun (n_indent, idx, idx_visual as acc) ->
+            function
+            | '\t' -> n_indent + tab_is_spaces, succ idx, None
+            | ' '  -> n_indent + 1            , succ idx, None
+            | '\n' | '\r' ->
+              failwith "Parse.Indentation_tree.count_indent_until_visual: \
+                        we only support a single line as input"
+            | _ ->
+              let res = n_indent, idx, Some idx in
+              raise @@ Shortcircuit res
+          ) (0, 0, None)
+        with Shortcircuit v -> v
+      in
+      n_indent, idx_visual
+
+    let add_line ~tab_is_spaces line tree =
+      (*> Note that n-indent is not the same as index
+        * tab is counted a specified number of spaces
+        * when supporting unicode in the future, whitespace can be of even more sizes
+      *)
+      let n_indent, idx_visual =
+        count_indent_until_visual ~tab_is_spaces line in
+      failwith "todo"
+
   end
 
 end
