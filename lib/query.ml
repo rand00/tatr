cf4c80d3dba7ca293b9d052caeb1c4db1693bfe3
rand00 <oth.rand@gmail.com>
2026-02-19 23:11:33 +0100

    lib/Query: Cleanup of old comments

--- lib/query.ml
+++ lib/query.ml
@@ -19,8 +19,6 @@ module Tree = struct
   let match_fulltree tree : _ Tree.t =
     failwith "todo"
 
-  (*> goto goo; fix that each tag-matcher should match at least _once_ per
-      matched branch in returned tree *)
   let match_matchtree (query:t) tree : _ Tree.t =
     let rec is_complete = function
       | Nil -> false
@@ -31,8 +29,6 @@ module Tree = struct
     let rec aux ancestor_matched acc_path_matches = function
       | Nil -> Nil
       | Tree (v, children) ->
-        (* CCFormat.eprintf "DEBUG: words = %a\n%!" *)
-        (*   (CCList.pp CCString.pp) v.Line_data.words; *)
         let matches, non_matches =
           query |> RSet.partition (fun regex ->
             v.Line_data.words |> CCList.exists regex.Tag_regex.prop 
@@ -42,7 +38,6 @@ module Tree = struct
         (*> Note: we need to continue to look even when complete already*)
         let path_is_complete = RSet.(cardinal acc_path_matches = cardinal query) in
         let found_match = not @@ RSet.is_empty matches in
-        (* CCFormat.eprintf "DEBUG: found-match = %b\n%!" found_match; *)
         if found_match then (
           let matching_children =
             aux_children true acc_path_matches children
