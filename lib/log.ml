795bc565100f832ecd7c46bb0e25f086928518f4
rand00 <oth.rand@gmail.com>
2026-04-08 18:41:01 +0200

    lib/Log: Added 'warn'

--- lib/log.ml
+++ lib/log.ml
@@ -3,4 +3,8 @@ let failwith ?(exitcode=1) scope msg =
   CCFormat.eprintf "@{<red>Error: %s:@} %s\n%!" scope msg;
   exit exitcode
 
+let warn scope msg =
+  CCFormat.eprintf "@{<red>Warning: %s:@} %s\n%!" scope msg
+
+
 
